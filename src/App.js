0d6059fb22d9cd9cf27457dc998c9b509af279ed
Abhang Paturkar <155937375+AbhangPaturkar2023BIT508@users.noreply.github.com>
2024-12-21 16:18:13 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
+
+
+function App() {
+  return (
+    <MantineProvider>
+      Hello
+    </MantineProvider>
+  );
+}
+
+export default App;
