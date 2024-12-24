56d92c6127116f0fca8fbe18a442daf76bf3cdb9
Abhang Paturkar <155937375+AbhangPaturkar2023BIT508@users.noreply.github.com>
2024-12-25 01:44:49 +0530

    Completed all components of homepage

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -2,7 +2,10 @@ import Header from "../Header/Header";
 import Companies from "../LandingPage/Companies";
 import DreamJob from "../LandingPage/DreamJob";
 import JobCategory from "../LandingPage/JobCategory";
+import Testimonials from "../LandingPage/Testimonials";
 import Working from "../LandingPage/Working";
+import Subscribe from "../LandingPage/Subscribe";
+import Footer from "../Footer/Footer";
 const Homepage = () => {
   return (
     <div className="min-h-[100vh] bg-mine-shaft-950 font-['poppins']">
@@ -11,6 +14,9 @@ const Homepage = () => {
       <Companies />
       <JobCategory />
       <Working />
+      <Testimonials />
+      <Subscribe />
+      <Footer />
     </div>
   );
 };
