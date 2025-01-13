36b9ee9a1a85839e860196d5f6e03350c54d6379
Abhang Paturkar <155937375+AbhangPaturkar2023BIT508@users.noreply.github.com>
2025-01-14 03:02:28 +0530

    User signup and login services - integrate with backend

--- src/Components/SignupLogin/Login.jsx
+++ src/Components/SignupLogin/Login.jsx
@@ -1,12 +1,33 @@
 import { Button, PasswordInput, rem, TextInput } from "@mantine/core";
 import { IconAt, IconLock } from "@tabler/icons-react";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { loginUser } from "../../Services/UserService";
+
+const form = {
+  email: "",
+  password: "",
+};
+
 const Login = () => {
+  const [data, setData] = useState(form);
+
+  const handleChange = (event) => {
+    setData({ ...data, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = () => {
+    loginUser(data)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err.response.data));
+  };
+
   return (
     <div className="w-1/2 px-20 flex flex-col justify-center gap-3">
       <div className="text-2xl font-semibold">Create Account</div>
       <TextInput
+        name="email"
+        onChange={handleChange}
         withAsterisk
         leftSection={
           <IconAt style={{ width: rem(18), height: rem(16) }} stroke={1.5} />
@@ -15,6 +36,8 @@ const Login = () => {
         placeholder="Your Email"
       />
       <PasswordInput
+        name="password"
+        onChange={handleChange}
         withAsterisk
         leftSection={
           <IconLock style={{ widht: rem(18), height: rem(18) }} stroke={1.5} />
@@ -22,12 +45,12 @@ const Login = () => {
         label="Password"
         placeholder="Password"
       />
-      <Button autoContrast variant="filled">
+      <Button onClick={handleSubmit} autoContrast variant="filled">
         Sign in
       </Button>
       <div className="mx-auto">
         Don't Have an account?
-        <Link to="/signup" className="text-bright-sun-400 hover:underline">
+        <Link to="/signup" className="text-bright-sun-400 hover:underline pl-1">
           SignUp
         </Link>
       </div>
--- src/Components/SignupLogin/Signup.jsx
+++ src/Components/SignupLogin/Signup.jsx
@@ -2,20 +2,55 @@ import {
   Anchor,
   Button,
   Checkbox,
+  Group,
   PasswordInput,
+  Radio,
   rem,
   TextInput,
 } from "@mantine/core";
 import { IconAt, IconLock } from "@tabler/icons-react";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { registerUser } from "../../Services/UserService";
+
+const form = {
+  name: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+  accountType: "APPLICANT",
+};
 
 const Signup = () => {
+  // const [value, setValue] = useState("APPLICANT");
+  const [data, setData] = useState(form);
+  
+  const handleChange = (event) => {
+    if (typeof event === "string") setData({ ...data, accountType: event });
+    else setData({ ...data, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = () => {
+    registerUser(data)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div className="w-1/2 px-20 flex flex-col justify-center gap-3">
       <div className="text-2xl font-semibold">Create Account</div>
-      <TextInput withAsterisk label="Full Name" placeholder="Your name" />
       <TextInput
+        value={data.name}
+        onChange={handleChange}
+        name="name"
+        withAsterisk
+        label="Full Name"
+        placeholder="Your name"
+      />
+      <TextInput
+        value={data.email}
+        onChange={handleChange}
+        name="email"
         withAsterisk
         leftSection={
           <IconAt style={{ width: rem(18), height: rem(16) }} stroke={1.5} />
@@ -24,6 +59,9 @@ const Signup = () => {
         placeholder="Your Email"
       />
       <PasswordInput
+        value={data.password}
+        name="password"
+        onChange={handleChange}
         withAsterisk
         leftSection={
           <IconLock style={{ widht: rem(18), height: rem(18) }} stroke={1.5} />
@@ -32,6 +70,9 @@ const Signup = () => {
         placeholder="Password"
       />
       <PasswordInput
+        name="confirmPassword"
+        value={data.confirmPassword}
+        onChange={handleChange}
         withAsterisk
         leftSection={
           <IconLock style={{ widht: rem(18), height: rem(18) }} stroke={1.5} />
@@ -39,6 +80,28 @@ const Signup = () => {
         label="Confirm Password"
         placeholder="Confirm Password"
       />
+      <Radio.Group
+        value={data.accountType}
+        onChange={handleChange}
+        label="You are ?"
+        withAsterisk
+      >
+        <Group mt="xs">
+          <Radio
+            className="py-4 px-6 hover:bg-mine-shaft-900 has-[:checked]:bg-bright-sun-400/5 has-[:checked]:border-bright-sun-400 border-mine-shaft-800 rounded-lg"
+            autoContrast
+            value="APPLICANT"
+            label="Applicant"
+          />
+          <Radio
+            className="py-4 px-6 hover:bg-mine-shaft-900 has-[:checked]:bg-bright-sun-400/5 has-[:checked]:border-bright-sun-400 border-mine-shaft-800 rounded-lg"
+            autoContrast
+            value="EMPLOYER"
+            label="Employer"
+          />
+        </Group>
+      </Radio.Group>
+
       <Checkbox
         autoContrast
         label={
@@ -47,12 +110,12 @@ const Signup = () => {
           </>
         }
       />
-      <Button autoContrast variant="filled">
+      <Button onClick={handleSubmit} autoContrast variant="filled">
         Sign up
       </Button>
       <div className="mx-auto">
         Have an account?
-        <Link to="/login" className="text-bright-sun-400 hover:underline">
+        <Link to="/login" className="text-bright-sun-400 hover:underline pl-1">
           Login
         </Link>
       </div>
