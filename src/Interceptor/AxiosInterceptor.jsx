afb7e81705165bd913b07182af0553f1e680800c
Abhang Paturkar <155937375+AbhangPaturkar2023BIT508@users.noreply.github.com>
2025-05-26 01:36:24 +0530

    Package json modify

--- src/Interceptor/AxiosInterceptor.jsx
+++ src/Interceptor/AxiosInterceptor.jsx
@@ -2,7 +2,8 @@ import axios from "axios";
 import { navigateToLogin } from "../Services/AuthService";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080",
+  // baseURL: "http://localhost:8080",
+  baseURL: "https://jobportal-backend-dnyd.onrender.com",
 });
 
 axiosInstance.interceptors.request.use(
